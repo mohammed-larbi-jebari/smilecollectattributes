ae347fd1396e04b8a74cbef5cd56c58f26e93870
Mohammed l'arbi Jebari <arbito82@gmail.com>
2014-07-22 15:28:27 +0100

    move files

---
+++ autoloads/eztemplateautoload.php
@@ -0,0 +1,12 @@
+<?php
+
+// Operator autoloading
+
+$eZTemplateOperatorArray = array();
+
+$eZTemplateOperatorArray[] =
+   array( 'script' => 'extension/smilecollectattributes/autoloads/smiletemplateparsexmloperator.php',
+                                    'class' => 'smileTemplateParseXMLOperator',
+                                    'operator_names' => array( 'parsexml' ) );
+
+?>
