606a13ac50666e0c48174be1fae8c582c649e2dc
Mohammed l'arbi Jebari <arbito82@gmail.com>
2014-07-22 15:26:16 +0100

    move files

--- smilecollectattributes/settings/content.ini.append.php
+++
@@ -1,8 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[DataTypeSettings]
-ExtensionDirectories[]=smilecollectattributes
-AvailableDataTypes[]=smilematrix
-AvailableDataTypes[]=smileobjectrelationlist
-AvailableDataTypes[]=smilebinaryfile
-*/ ?>
--- smilecollectattributes/settings/site.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[TemplateSettings]
-ExtensionAutoloadPath[]=smilecollectattributes
-
-[RegionalSettings]
-TranslationExtensions[]=smilecollectattributes
-
-*/ ?>
